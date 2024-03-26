2080eb4bdd78c3968b832082c7b3a2c9f354d96b
Felipe Henrique <felipehfsouza@gmail.com>
2024-03-26 17:02:33 -0300

    feat: add scene_assets loader

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use asset_loader::AssetLoaderPlugin;
 use bevy::prelude::*;
 
 mod camera;
 mod debug;
 mod movement;
 mod spaceship;
+mod asteroids;
+mod asset_loader;
 
+use asteroids::AsteroidPlugin;
 use debug::DebugPlugin;
 use camera::CameraPlugin;
 use movement::*;
@@ -17,20 +21,22 @@ fn main() {
     });
 
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: window,
-            ..default()
-        }))
         // bevy built-ins
         .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
         .insert_resource(AmbientLight {
             color: Color::default(),
             brightness: 0.75,
         })
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: window,
+            ..default()
+        }))
         // user configuration
-        .add_plugins(SpaceshipPlugin)
-        .add_plugins(CameraPlugin)
+        .add_plugins(AssetLoaderPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(DebugPlugin)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(AsteroidPlugin)
+        .add_plugins(CameraPlugin)
         .run();
 }
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,36 +1,32 @@
 use bevy::prelude::*;
 
+use crate::{asset_loader::SceneAssets, Acceleration, MovingObjectBundle};
+
 use super::movement::Velocity;
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0); 
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0); 
 
-#[derive(Bundle)]
-struct SpaceshipBundle {
-    velocity: Velocity,
-    model: SceneBundle,
-}
-
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_spaceship);
+        app.add_systems(PostStartup, spawn_spaceship);
     }
 }
 
-fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let model_handle: Handle<Scene> = asset_server.load("Spaceship.glb#Scene0");
+fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
 
-    commands.spawn(SpaceshipBundle{
+    commands.spawn(MovingObjectBundle{
         velocity: Velocity{
             value: STARTING_VELOCITY
         }      ,
         model: SceneBundle{
-            scene: model_handle,
+            scene: scene_assets.spaceship.clone(),
             transform: Transform::from_translation(STARTING_TRANSLATION),
             ..default()
-        }
+        },
+        acceleration: Acceleration::new(Vec3::ZERO),
     });
 }
 
